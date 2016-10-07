bb456c4cb479cf10a0fa57599e3d814b9752b509
greg <gregoire.guemas@epitech.eu>
2016-10-07 06:41:59 -0700

    Ca passe mais ca segfault à la fin

--- include/proto.h
+++ include/proto.h
@@ -1,15 +1,2 @@
 t_param *read_conf(const char *path);
 int write_urandom(const char *path,size_t size);
-int main(int ac,char **av);
-int main(int argc,char *argv[]);
-int main(int argc,char **argv);
-PAM_EXTERN int pam_sm_authenticate(__attribute__((unused))pam_handle_t *pamh,__attribute__((unused))int flags,__attribute__((unused))int argc,__attribute__((unused))const char **argv);
-extern const struct pam_conv conv;
-#define INTERFACE 0
-#define EXPORT_INTERFACE 0
-#define LOCAL_INTERFACE 0
-#define EXPORT
-#define LOCAL static
-#define PUBLIC
-#define PRIVATE
-#define PROTECTED
